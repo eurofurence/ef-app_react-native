8996c2e651b8b576fbc340ac3975e15a5c89fa6f
requinard <david@requinard.nl>
2022-07-03 13:16:17 +0200

    add basic implementation of cloud messaging

--- src/components/Notifications/NotificationManager.tsx
+++ src/components/Notifications/NotificationManager.tsx
@@ -1,9 +1,14 @@
+import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Platform } from "react-native";
 
 import { withPlatform } from "../../hoc/withPlatform";
+import { notificationsSlice } from "../../store/notifications.slice";
 
 export const NotificationManager = () => {
+    const [expoPushToken, setExpoPushToken] = useState("");
+
     useEffect(() => {
         Notifications.setNotificationHandler({
             handleNotification: async () => ({
@@ -14,7 +19,51 @@ export const NotificationManager = () => {
         });
     });
 
+    useEffect(() => {
+        registerForPushNotifications().then((token) => setExpoPushToken(token));
+
+        const notificationHAndlerSubscription = Notifications.addNotificationReceivedListener(handleNotification);
+
+        return () => {
+            Notifications.removeNotificationSubscription(notificationHAndlerSubscription);
+        };
+    }, []);
+
     return null;
+};
+
+const handleNotification = (notification: Notifications.Notification) => {
+    console.debug("Received a notification", notification.request.identifier, notification);
+};
+
+const registerForPushNotifications = async (): Promise<string> => {
+    if (!Device.isDevice) {
+        alert("A physical device is required for Push Notifications.");
+        return "";
+    }
+
+    const status = await Notifications.getPermissionsAsync();
+
+    if (status.status !== "granted") {
+        const newStatus = await Notifications.requestPermissionsAsync();
+
+        if (newStatus.status !== "granted") {
+            alert("We are not permitted to receive notifications!");
+            return "";
+        }
+    }
+    const token = await Notifications.getDevicePushTokenAsync();
+    console.debug("expo token", token);
+
+    if (Platform.OS === "android") {
+        Notifications.setNotificationChannelAsync("default", {
+            name: "default",
+            importance: Notifications.AndroidImportance.MIN,
+            lightColor: "#006459",
+        });
+    }
+
+    return token.data;
 };
 
 export const PlatformNotificationManager = withPlatform(NotificationManager, ["android", "ios"]);
