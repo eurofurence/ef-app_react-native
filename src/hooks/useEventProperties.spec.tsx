b192e4d21da53d406d88bd42ef847b7126c8c22f
requinard <david@requinard.nl>
2022-07-15 12:25:31 +0200

    add tests for event properties hooks

---
+++ src/hooks/useEventProperties.spec.tsx
@@ -0,0 +1,72 @@
+import moment from "moment";
+
+import { renderHook } from "../testUtils";
+import { useEventIsDone, useEventIsHappening } from "./useEventProperties";
+
+describe("useEventProperties", function () {
+    describe("useEventIsHappening", () => {
+        it("returns true if an event is happening now", () => {
+            const event = {
+                StartDateTimeUtc: moment().subtract(1, "hour").toISOString(),
+                EndDateTimeUtc: moment().add(1, "hour").toISOString(),
+            };
+            const { result } = renderHook(() => useEventIsHappening(event), {});
+
+            expect(result.current).toBeTruthy();
+        });
+
+        it("returns false if an event is in the past", () => {
+            const event = {
+                StartDateTimeUtc: moment().subtract(2, "hour").toISOString(),
+                EndDateTimeUtc: moment().subtract(1, "hour").toISOString(),
+            };
+            const { result } = renderHook(() => useEventIsHappening(event), {});
+
+            expect(result.current).toBeFalsy();
+        });
+
+        it("returns false if an event is in the future", () => {
+            const event = {
+                StartDateTimeUtc: moment().add(1, "hour").toISOString(),
+                EndDateTimeUtc: moment().add(2, "hour").toISOString(),
+            };
+            const { result } = renderHook(() => useEventIsHappening(event), {});
+
+            expect(result.current).toBeFalsy();
+        });
+    });
+
+    describe("useEventIsDone", function () {
+        it("returns true for an event in the past", () => {
+            const event = {
+                EndDateTimeUtc: moment().subtract(1, "hour").toISOString(),
+            };
+
+            const { result } = renderHook(() => useEventIsDone(event), {});
+
+            expect(result.current).toBeTruthy();
+        });
+
+        it("returns false for an event in the that is still ongoing", () => {
+            const event = {
+                StartDateTimeUtc: moment().subtract(1, "hour").toISOString(),
+                EndDateTimeUtc: moment().add(1, "hour").toISOString(),
+            };
+
+            const { result } = renderHook(() => useEventIsDone(event), {});
+
+            expect(result.current).toBeFalsy();
+        });
+
+        it("returns false for an event in the future", () => {
+            const event = {
+                StartDateTimeUtc: moment().add(1, "day").toISOString(),
+                EndDateTimeUtc: moment().add(1, "hour").add(1, "day").toISOString(),
+            };
+
+            const { result } = renderHook(() => useEventIsDone(event), {});
+
+            expect(result.current).toBeFalsy();
+        });
+    });
+});
--- src/hooks/useEventProperties.tsx
+++ src/hooks/useEventProperties.tsx
@@ -3,11 +3,11 @@ import { useMemo } from "react";
 import { EventRecord } from "../store/eurofurence.types";
 import { useNow } from "./useNow";
 
-export const useEventIsHappening = (event: EventRecord) => {
+export const useEventIsHappening = (event: Pick<EventRecord, "StartDateTimeUtc" | "EndDateTimeUtc">) => {
     const [now] = useNow();
     return useMemo(() => now.isBetween(event.StartDateTimeUtc, event.EndDateTimeUtc), [now, event]);
 };
-export const useEventIsDone = (event: EventRecord) => {
+export const useEventIsDone = (event: Pick<EventRecord, "EndDateTimeUtc">) => {
     const [now] = useNow();
     return useMemo(() => now.isAfter(event.EndDateTimeUtc), [now, event]);
 };
