d8261e645573496e1960e292b7506800caf63d2b
requinard <david@requinard.nl>
2022-08-22 09:20:59 +0200

    remove old API calls from the eurofurence service

--- src/store/eurofurence.service.ts
+++ src/store/eurofurence.service.ts
@@ -1,23 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import _ from "lodash";
-import { REHYDRATE } from "redux-persist";
 
 import { apiBase } from "../configuration";
-import {
-    AnnouncementRecord,
-    CommunicationRecord,
-    DealerRecord,
-    EventDayRecord,
-    EventRecord,
-    EventRoomRecord,
-    EventTrackRecord,
-    ImageRecord,
-    KnowledgeEntryRecord,
-    KnowledgeGroupRecord,
-    MapRecord,
-    RecordId,
-    RecordMetadata,
-} from "./eurofurence.types";
+import { CommunicationRecord, RecordId, RecordMetadata } from "./eurofurence.types";
 
 const tagsFromList =
     <TagType extends string>(type: TagType) =>
@@ -42,90 +27,8 @@ export const eurofurenceService = createApi({
             return headers;
         },
     }),
-    tagTypes: ["Announcement", "Event", "Dealer", "EventDay", "EventTrack", "EventRoom", "Map", "KnowledgeGroup", "KnowledgeEntry", "Image", "Communication"],
-    keepUnusedDataFor: 0,
-    extractRehydrationInfo(action, { reducerPath }) {
-        if (action.type === REHYDRATE && action.payload) {
-            return action.payload[reducerPath];
-        }
-    },
+    tagTypes: ["Communication"],
     endpoints: (builder) => ({
-        getAnnouncements: builder.query<AnnouncementRecord[], void>({
-            query: () => ({ url: "/Announcements" }),
-            providesTags: tagsFromList("Announcement"),
-        }),
-        getAnnouncementsById: builder.query<AnnouncementRecord, RecordId>({
-            query: (args) => ({ url: `/Announcements/${args}` }),
-            providesTags: tagsFromItem("Announcement"),
-        }),
-        getEvents: builder.query<EventRecord[], void>({
-            query: () => ({ url: "/Events" }),
-            providesTags: tagsFromList("Event"),
-        }),
-        getEventById: builder.query<EventRecord, RecordId>({
-            query: (args) => ({ url: `/Events/${args}` }),
-            providesTags: tagsFromItem("Event"),
-        }),
-        getDealers: builder.query<DealerRecord[], void>({
-            query: () => ({ url: "/Dealers" }),
-            providesTags: tagsFromList("Dealer"),
-        }),
-        getDealerById: builder.query<DealerRecord, RecordId>({
-            query: (args) => ({ url: `/Dealers/${args}` }),
-            providesTags: tagsFromItem("Dealer"),
-        }),
-        getEventDays: builder.query<EventDayRecord[], void>({
-            query: () => ({ url: "/EventConferenceDays" }),
-            providesTags: tagsFromList("EventDay"),
-        }),
-        getEventDayById: builder.query<EventDayRecord, RecordId>({
-            query: (args) => ({ url: `/EventConferenceDays/${args}` }),
-            providesTags: tagsFromItem("EventDay"),
-        }),
-        getEventTracks: builder.query<EventTrackRecord[], void>({
-            query: () => ({ url: "/EventConferenceTracks" }),
-            providesTags: tagsFromList("EventTrack"),
-        }),
-        getEventTrackById: builder.query<EventTrackRecord, RecordId>({
-            query: (args) => ({ url: `/EventConferenceTracks/${args}` }),
-            providesTags: tagsFromItem("EventTrack"),
-        }),
-        getEventRooms: builder.query<EventRoomRecord[], void>({
-            query: () => ({ url: "/EventConferenceRooms" }),
-            providesTags: tagsFromList("EventRoom"),
-        }),
-        getEventRoomById: builder.query<EventRoomRecord, RecordId>({
-            query: (args) => ({ url: `/EventConferenceRooms/${args}` }),
-            providesTags: tagsFromItem("EventRoom"),
-        }),
-        getMaps: builder.query<MapRecord[], void>({
-            query: () => ({ url: "/Maps" }),
-            providesTags: tagsFromList("Map"),
-        }),
-        getMapById: builder.query<MapRecord, RecordId>({
-            query: (args) => ({ url: `/Maps/${args}` }),
-            providesTags: tagsFromItem("Map"),
-        }),
-        getKnowledgeGroups: builder.query<KnowledgeGroupRecord[], void>({
-            query: () => ({ url: "/KnowledgeGroups" }),
-            providesTags: tagsFromList("KnowledgeGroup"),
-        }),
-        getKnowledgeGroupById: builder.query<KnowledgeGroupRecord, RecordId>({
-            query: (args) => ({ url: `/KnowledgeGroups/${args}` }),
-            providesTags: tagsFromItem("KnowledgeGroup"),
-        }),
-        getKnowledgeEntries: builder.query<KnowledgeEntryRecord[], void>({
-            query: () => ({ url: "/KnowledgeEntries" }),
-            providesTags: tagsFromList("KnowledgeEntry"),
-        }),
-        getKnowledgeEntryById: builder.query<KnowledgeEntryRecord, RecordId>({
-            query: (args) => ({ url: `/KnowledgeEntries/${args}` }),
-            providesTags: tagsFromItem("KnowledgeEntry"),
-        }),
-        getImages: builder.query<ImageRecord[], void>({
-            query: () => ({ url: `/Images` }),
-            providesTags: tagsFromList("Image"),
-        }),
         getCommunications: builder.query<CommunicationRecord[], void>({
             query: () => "/Communication/PrivateMessages",
             providesTags: tagsFromList("Communication"),
@@ -157,19 +60,4 @@ export const eurofurenceService = createApi({
     }),
 });
 
-export const {
-    useGetAnnouncementsQuery,
-    useGetEventsQuery,
-    useGetEventByIdQuery,
-    useGetDealersQuery,
-    useGetImagesQuery,
-    useGetEventDaysQuery,
-    useGetEventDayByIdQuery,
-    useGetEventTracksQuery,
-    useGetEventTrackByIdQuery,
-    useGetEventRoomsQuery,
-    useGetEventRoomByIdQuery,
-    useGetCommunicationsQuery,
-    useMarkCommunicationReadMutation,
-    useSubmitEventFeedbackMutation,
-} = eurofurenceService;
+export const { useGetCommunicationsQuery, useMarkCommunicationReadMutation, useSubmitEventFeedbackMutation } = eurofurenceService;
