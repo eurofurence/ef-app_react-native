6eec75ca0ea6623b5c5103c566f6301bf6f0b0e3
requinard <david@requinard.nl>
2022-08-30 20:59:13 +0200

    prep for storyshot

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,8 @@
 import mockAsyncStorage from "@react-native-async-storage/async-storage/jest/async-storage-mock";
 import "@testing-library/jest-native/extend-expect";
 
+import initStoryshots from "@storybook/addon-storyshots";
+
 jest.mock("@react-native-async-storage/async-storage", () => mockAsyncStorage);
 jest.useFakeTimers();
+// initStoryshots();
