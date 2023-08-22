cc425a0412e5872096819bebc3930b2efb9c3068
Lukas Haertel <lukashaertel@gmail.com>
2023-08-22 19:26:09 +0200

    Minor fixes
    * Placeholder for posters
    * Missing estimated size.
    * Subscription remove instead of remove handler.
    * Filtering header for maps reworked, bigger part of bottom tab.
    * Language picker button down changed in color.
    * Refresh key simplified for events.

--- src/hooks/useEventsRefreshKey.ts
+++ src/hooks/useEventsRefreshKey.ts
@@ -1,14 +1,10 @@
-import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
-
-import { useNowRefreshKey } from "./useNowRefreshKey";
 
 /**
  * Uses a key that can be used for refreshing events.
  * @param nowInterval The interval of the time slotting key.
  */
 export const useEventsRefreshKey = (nowInterval = 5) => {
-    const { t } = useTranslation("Event");
-    const nowRefreshKey = useNowRefreshKey(nowInterval);
-    return useMemo(() => [t, nowRefreshKey], [t, nowRefreshKey]);
+    const { t } = useTranslation("Dealer");
+    return t;
 };
