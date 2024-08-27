568d27f7e892b460148c2249f7e53c6744509bde
Fenrikur <3359222+Fenrikur@users.noreply.github.com>
2024-08-27 18:58:49 +0200

    fix: compare time zone offset instead of name (#139)

--- src/components/home/TimezoneWarning.tsx
+++ src/components/home/TimezoneWarning.tsx
@@ -1,4 +1,5 @@
 import { useCalendars } from "expo-localization";
+import moment from "moment-timezone";
 import React, { FC } from "react";
 import { useTranslation } from "react-i18next";
 
@@ -16,13 +17,17 @@ export const TimezoneWarning: FC<TimezoneWarningProps> = ({ parentPad = 0 }) => 
     const { t } = useTranslation("Home");
     const { timeZone } = useCalendars()[0];
 
-    if (conTimeZone === timeZone) {
+    const now = new Date();
+    const conTimeZoneOffset = moment.tz(now, conTimeZone).utcOffset();
+    const deviceTimeZoneOffset = moment.tz(now, timeZone ?? conTimeZone).utcOffset();
+
+    if (conTimeZoneOffset === deviceTimeZoneOffset) {
         return null;
     }
 
     return (
         <Badge unpad={parentPad} badgeColor="background" textColor="text" textType="para" icon="clock">
-            {t("different_timezone", { convention: conName })}
+            {t("different_timezone", { convention: conName, conTimeZone, deviceTimeZone: timeZone })}
         </Badge>
     );
 };
