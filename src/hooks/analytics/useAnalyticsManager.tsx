96b7bfcf7631df58c8cadb62d698b87201be6dc7
requinard <david@requinard.nl>
2024-09-02 21:35:37 +0200

    make sure user is not loaded when sentry data is sent

--- src/hooks/analytics/useAnalyticsManager.tsx
+++ src/hooks/analytics/useAnalyticsManager.tsx
@@ -1,10 +1,10 @@
 import analytics from "@react-native-firebase/analytics";
-import { captureException, setTag, setUser } from "@sentry/react-native";
+import {captureException, setTag, setUser} from "@sentry/react-native";
 import Constants from "expo-constants";
-import { useEffect } from "react";
+import {useEffect} from "react";
 
-import { useAuthContext } from "../../context/AuthContext";
-import { useAppSelector } from "../../store";
+import {useAuthContext} from "../../context/AuthContext";
+import {useAppSelector} from "../../store";
 
 /**
  * Manages data collection settings transfer. Sets up a connection for user and
@@ -13,7 +13,7 @@ import { useAppSelector } from "../../store";
  */
 export const useAnalyticsManager = () => {
     // Get user and analytics-enabled status from app store.
-    const { claims } = useAuthContext();
+    const {claims} = useAuthContext();
     const enabled = useAppSelector((state) => state.settingsSlice.analytics.enabled);
 
     // Enable analytics collection if enabled-flag reads true from the app store.
@@ -23,20 +23,22 @@ export const useAnalyticsManager = () => {
 
     useEffect(() => {
         // Set user for sentry, if user is given.
-        if (claims) setUser({ id: claims.uid as string, username: claims.name as string });
-        else setUser(null);
+        if (claims && enabled) {
+            setUser({id: claims?.uid as string, username: claims.name as string})
+            // Initialize firebase analytics user if given.
+            analytics()
+                .setUserId((claims?.uid ?? null) as string | null)
+                .catch(captureException);
+
+            // Initialize firebase username if given.
+            analytics()
+                .setUserProperty("username", (claims?.name ?? null) as string | null)
+                .catch(captureException);
+        } else {
+            setUser(null)
+        }
 
         // Set app version code.
         setTag("appVersionCode", Constants.expoConfig?.android?.versionCode);
-
-        // Initialize firebase analytics user if given.
-        analytics()
-            .setUserId((claims?.uid ?? null) as string | null)
-            .catch(captureException);
-
-        // Initialize firebase username if given.
-        analytics()
-            .setUserProperty("username", (claims?.name ?? null) as string | null)
-            .catch(captureException);
-    }, [claims]);
+    }, [claims, enabled]);
 };
