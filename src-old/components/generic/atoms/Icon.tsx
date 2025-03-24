3d868798ac8ef6ead9402266d45caa54b59cfcef
Faye <faye@fayevr.dev>
2025-03-24 16:55:05 +0100

    delete src-old

--- src-old/components/generic/atoms/Icon.tsx
+++
@@ -1,8 +0,0 @@
-import MaterialCommunityIcon from "@expo/vector-icons/MaterialCommunityIcons";
-import { Platform } from "react-native";
-
-export type IconNames = keyof typeof MaterialCommunityIcon.glyphMap;
-
-export const Icon = MaterialCommunityIcon;
-
-export const platformShareIcon: IconNames = Platform.OS === "ios" ? "export-variant" : "share";
--- src-old/init/firebaseApp.ts
+++
@@ -1,7 +0,0 @@
-import type { FirebaseApp } from "firebase/app";
-
-/**
- * Stub for Firebase JS framework, skipped when using the react-native
- * integration.
- */
-export const firebaseApp: FirebaseApp = null as any;
