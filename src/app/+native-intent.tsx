e0852bef2296fa3b236c48260b7243edcb6b8f3f
Fenrikur <3359222+Fenrikur@users.noreply.github.com>
2025-08-18 01:05:03 +0200

    fix: deep linking to Artist Alley and KB

--- src/app/+native-intent.tsx
+++ src/app/+native-intent.tsx
@@ -1,10 +1,18 @@
 import { appBase } from '@/configuration'
 
-const prefixEvents = `${appBase}/Web/Events/`
+const matchArtistAlley = `${appBase}/Web/ArtistAlley`
+const matchArtistAlleyReg = `${appBase}/Web/ArtistAlleyReg`
+const matchKnowledgeGroups = `${appBase}/Web/KnowledgeGroups`
 const prefixDealers = `${appBase}/Web/Dealers/`
+const prefixEvents = `${appBase}/Web/Events/`
+const prefixKnowledgeEntries = `${appBase}/Web/KnowledgeEntries/`
 
 export function redirectSystemPath({ path }: { path: string }) {
   if (path.startsWith(prefixEvents)) return `/events/${path.substring(prefixEvents.length)}`
   if (path.startsWith(prefixDealers)) return `/dealers/${path.substring(prefixDealers.length)}`
+  if (path.startsWith(prefixKnowledgeEntries)) return `/knowledge/${path.substring(prefixKnowledgeEntries.length)}`
+  if (path === matchArtistAlley) return `/artists-alley`
+  if (path === matchArtistAlleyReg) return `/artists-alley/reg`
+  if (path === matchKnowledgeGroups) return `/knowledge`
   return path
 }
