0858da70fcdd032ed4b5fd1ca63699bcedd64732
Faye <faye@fayevr.dev>
2025-09-25 11:43:52 +0200

    fix: throw deprecation FileSystem

--- src/hooks/api/artists-alley/postArtistsAlleyPostTableRegistrationRequest.ts
+++ src/hooks/api/artists-alley/postArtistsAlleyPostTableRegistrationRequest.ts
@@ -1,5 +1,5 @@
-import * as FileSystem from 'expo-file-system'
-import { FileSystemSessionType, FileSystemUploadType } from 'expo-file-system'
+import { Paths } from 'expo-file-system'
+import { FileSystemSessionType, FileSystemUploadType, uploadAsync, downloadAsync, deleteAsync } from 'expo-file-system/legacy'
 
 import { apiBase } from '@/configuration'
 import { ArtistsAlleyPostTableRegistrationData } from '@/hooks/api/artists-alley/ArtistsAlleyPostTableRegistrationData'
@@ -9,24 +9,26 @@ import { ArtistsAlleyPostTableRegistrationData } from '@/hooks/api/artists-alley
  * @param accessToken The access token.
  * @param data The registration data.
  */
+// TODO: This is a legacy function. It should be updated to use the new file system API.
 export async function postArtistsAlleyPostTableRegistrationRequest(accessToken: string | null, data: ArtistsAlleyPostTableRegistrationData) {
   if (!accessToken) throw new Error('Unauthorized')
 
   // Check if uploading a new image or an existing. If existing, download.
-  let downloadedUri
+  let downloadedUri: string | null = null
   if (data.imageUri.startsWith('file:')) {
     downloadedUri = null
   } else {
     const fileName = data.imageUri.substring(data.imageUri.lastIndexOf('/') + 1)
-    downloadedUri = FileSystem.cacheDirectory + fileName
-    await FileSystem.downloadAsync(data.imageUri, downloadedUri, {
+    downloadedUri = Paths.cache.uri + fileName
+    await downloadAsync(data.imageUri, downloadedUri, {
       sessionType: FileSystemSessionType.FOREGROUND,
     })
   }
 
   try {
     // Upload to registration. Return true.
-    await FileSystem.uploadAsync(`${apiBase}/ArtistsAlley/TableRegistrationRequest`, downloadedUri ?? data.imageUri, {
+    const fileToUpload = downloadedUri ?? data.imageUri
+    await uploadAsync(`${apiBase}/ArtistsAlley/TableRegistrationRequest`, fileToUpload, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
@@ -51,7 +53,7 @@ export async function postArtistsAlleyPostTableRegistrationRequest(accessToken: 
   } finally {
     // Clean up if a temporary file was downloaded.
     if (downloadedUri) {
-      await FileSystem.deleteAsync(downloadedUri)
+      await deleteAsync(downloadedUri)
     }
   }
 }
