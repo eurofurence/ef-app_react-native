6df2af7162dc36aa2bec1e3a4937fc9fb1284473
Faye <faye@fayevr.dev>
2026-07-14 14:19:10 +0200

    fix: telemetry fixes

--- src/util/redactSensitiveUrl.ts
+++ src/util/redactSensitiveUrl.ts
@@ -2,7 +2,7 @@
 export function redactSensitiveUrl<T>(url: T): T {
   if (typeof url !== 'string') return url
   return url.replace(
-    /([?&])(id|pw|token|password)=[^&#]*/gi,
+    /([?&#])(id|pw|token|password)=[^&#]*/gi,
     (_m, sep, key) => `${sep}${key}=REDACTED`
   ) as T
 }
