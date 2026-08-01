f734f384cec00225cf945cdc0f8c88ecff90bee4
Fenrikur <3359222+Fenrikur@users.noreply.github.com>
2026-08-01 18:13:55 +0200

    feat(events): deep-link to feedback

--- src/app/+native-intent.tsx
+++ src/app/+native-intent.tsx
@@ -8,14 +8,18 @@ const prefixDealers = `${appBase}/Web/Dealers/`
 const prefixEvents = `${appBase}/Web/Events/`
 const prefixKnowledgeEntries = `${appBase}/Web/KnowledgeEntries/`
 const prefixWifi = 'eurofurence://wifi'
+const uuidLength = 36
 
 export function redirectSystemPath({ path }: { path: string }) {
-  if (path.startsWith(prefixEvents))
-    return `/events/${path.substring(prefixEvents.length)}`
+  if (path.startsWith(prefixEvents)) {
+    if (path.endsWith('/Feedback')) 
+      return `/events/${path.substring(prefixEvents.length, prefixEvents.length + uuidLength)}/feedback`
+    return `/events/${path.substring(prefixEvents.length, prefixEvents.length + uuidLength)}`
+  }
   if (path.startsWith(prefixDealers))
-    return `/dealers/${path.substring(prefixDealers.length)}`
+    return `/dealers/${path.substring(prefixDealers.length, prefixDealers.length + uuidLength)}`
   if (path.startsWith(prefixKnowledgeEntries))
-    return `/knowledge/${path.substring(prefixKnowledgeEntries.length)}`
+    return `/knowledge/${path.substring(prefixKnowledgeEntries.length, prefixKnowledgeEntries.length + uuidLength)}`
   if (path === matchArtistAlley) return `/artists-alley`
   if (path === matchArtistAlleyReg) return `/artists-alley/reg`
   if (path === matchKnowledgeGroups) return `/knowledge`
